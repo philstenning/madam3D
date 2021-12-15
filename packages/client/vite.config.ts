ea5c053085094da076981f1929462d8ef64a616e
Phil Stenning <philstenning@hotmail.com>
2021-12-15 18:18:23 +0000

    bugfix: incorrect base url

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,5 +4,5 @@ import Icons from "unplugin-icons/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Icons({ compiler: "jsx", jsx: "react" })],
-  
+  base: "https://philstenning.github.io/madam3D/",
 });
