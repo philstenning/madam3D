42becbbf72d1db2e7b323a9d62eece8d8cca5618
Phil Stenning <philstenning@hotmail.com>
2022-01-19 10:24:36 +0000

    feature: change the quantity of previews shown Fixes #12

--- packages/client/src/components/modelList/ModelList.tsx
+++ packages/client/src/components/modelList/ModelList.tsx
@@ -1,22 +1,53 @@
-import { useState, useEffect, CSSProperties, useLayoutEffect } from "react";
+import { useState, useEffect, CSSProperties } from "react";
 import StlCard from "../stlCard/StlCard";
-import { IFolder, IFile, FileTypes, db } from "../../db";
+import { IFile, db } from "../../db";
 import "./modelList.css";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { setCursor } from "../../features/folderSlice";
-import { filterFolderFiles, recursivelyScanLocalDrive } from "../../utils";
+import { filterFolderFiles } from "../../utils";
 import { v4 as uuid } from "uuid";
 import Pagination, { paginate, sliceFiles } from "../pagination/Pagination";
-
 import useMeasure from "react-use-measure";
-type Props = {
-  folderId: string;
-};
+
+function calculateVariables(): {
+  style: { "--model-list-columns": number; "--model-list-rows": number };
+  listQuantity: number;
+} {
+  const { innerHeight, innerWidth } = window;
+  const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+  const calculatedWidth = Math.floor(innerWidth - 19.6 * rem);
+  const calculatedHeight = Math.floor(innerHeight - 8 * rem);
+  // console.log("width", calculatedWidth, innerWidth);
+  const heightMultiplier = calculatedHeight > 490 ? 2 : 1;
+  let style = {
+    "--model-list-columns": 4,
+    "--model-list-rows": heightMultiplier,
+  };
+  let listQuantity = 2;
+  if (calculatedWidth > 950) {
+    listQuantity = 3 * heightMultiplier;
+
+    style["--model-list-columns"] = 3;
+  } else if (calculatedWidth > 500) {
+    listQuantity = 2 * heightMultiplier;
+
+    style["--model-list-columns"] = 2;
+  } else if (calculatedWidth < 500) {
+    listQuantity = 1 * heightMultiplier;
+    style["--model-list-columns"] = 1;
+  }
+
+  return {
+    style,
+    listQuantity,
+  };
+}
 
 const ModelList = () => {
   const { cursor, currentFolder } = useAppSelector(
     (state) => state.folderReducer
   );
+
   const folderId = currentFolder?.id;
   const dispatch = useAppDispatch();
   // all files in the current folder
@@ -24,80 +55,24 @@ const ModelList = () => {
 
   // the files in the current paginated page
   const [currentPageFiles, setCurrentPageFiles] = useState<IFile[]>([]);
-  // cursor and limit are used to paginate the files
-  // const cursor = useAppSelector((state) => state.folderReducer.cursor);
+  // limit is used to paginate the files
   const [limit, setLimit] = useState(4);
-
+// used to inject the style into the modelList div.
   const [listStyle, setListStyle] = useState<CSSProperties>();
 
   const [ref, { width, height }] = useMeasure();
-
-  const calculateVariables = () => {
-    const { innerHeight, innerWidth } = window;
-    const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
-    console.log(innerHeight);
-    const calculatedWidth = Math.floor(innerWidth - 19.6 * rem);
-    const calculatedHeight = Math.floor(innerHeight - 8 * rem);
-
-    const heightMultiplier = calculatedHeight > 490 ? 2 : 1;
-    let style = {
-      "--model-list-columns": 4,
-      "--model-list-rows": heightMultiplier,
-    };
-    let listQuantity = 2;
-    if (calculatedWidth > 950) {
-      listQuantity = 3 * heightMultiplier;
-
-      style["--model-list-columns"] = 3;
-    } else if (calculatedWidth > 500) {
-      listQuantity = 2 * heightMultiplier;
-
-      style["--model-list-columns"] = 2;
-    } else if (calculatedWidth < 500) {
-      listQuantity = 1 * heightMultiplier;
-      style["--model-list-columns"] = 1;
-    }
-    // console.log(
-    //   "\n\n### before ",
-    //   "\n\twidth:",
-    //   width,
-    //   "\n\tcalculatedWidth",
-    //   calculatedWidth,
-    //   "\n\n\trem:",
-    //   rem,
-    //   "\n\n\tlimit",
-    //   limit,
-    //   "\n\tlistQuantity",
-    //   listQuantity,
-
-    //   "\n\n\theight:",
-    //   height,
-    //   "\n\tcalculatedHeight",
-    //   calculatedHeight,
-    //   "\n\n"
-    // );
-    setListStyle(style as CSSProperties);
-    return {
-      style,
-      calculatedHeight,
-      calculatedWidth,
-      heightMultiplier,
-      rem,
-      listQuantity,
-    };
-  };
-
+  /**
+   * when the veiwport changes dimensions, we need to recalculate the
+   * quantity of model items on view, this also need a css variable to
+   * be  updated.
+   */
   useEffect(() => {
-    const { listQuantity} = calculateVariables();
-
+    // filter the displayed files  when the cursor,height or width changes
+    const { listQuantity, style } = calculateVariables();
+    setListStyle(style as CSSProperties);
     setLimit(listQuantity);
-
-  
-    
-    // filter the displayed files  when the cursor changes
     const pageFiles = sliceFiles(allFiles, cursor, listQuantity);
     setCurrentPageFiles(pageFiles);
-  
   }, [cursor, width, height]);
 
   useEffect(() => {
@@ -112,7 +87,6 @@ const ModelList = () => {
           .first();
         if (!_current_folder) return;
         const _filteredFiles = await filterFolderFiles(_current_folder);
-
         // if we have a fileList, calculate the
         // number of pages for pagination
         if (_filteredFiles) {
@@ -123,7 +97,11 @@ const ModelList = () => {
             0,
             listQuantity
           );
-          const _pageFiles = sliceFiles(_filteredFiles, _newCursor, listQuantity);
+          const _pageFiles = sliceFiles(
+            _filteredFiles,
+            _newCursor,
+            listQuantity
+          );
           setCurrentPageFiles(_pageFiles);
           dispatch(setCursor(0));
         } else {
@@ -139,12 +117,7 @@ const ModelList = () => {
       {folderId && <Pagination countOfFiles={allFiles.length} limit={limit} />}
 
       {folderId && (
-        <div
-          ref={ref}
-          //  style={{gridTemplateColumns:`repeat(${templateCols}, 1fr)`}}
-          style={listStyle}
-          className="model-list"
-        >
+        <div ref={ref} style={listStyle} className="model-list">
           {currentPageFiles?.map((file) => (
             <StlCard key={uuid()} file={file} />
           ))}
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,15 +3,14 @@ import react from "@vitejs/plugin-react";
 import Icons from "unplugin-icons/vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  if(mode === "production") {
-    return{
+  if (mode === "production") {
+    return {
       base: "/madam3D/",
       plugins: [react(), Icons({ compiler: "jsx", jsx: "react" })],
-
-    }
+    };
   }
   return {
     plugins: [react(), Icons({ compiler: "jsx", jsx: "react" })],
-
+   
   };
 });
