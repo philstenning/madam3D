8dc68254e5a0e25a8b99f60060c550f382ffbf24
Phil Stenning <philstenning@hotmail.com>
2022-03-03 16:57:39 +0000

    update home page with logo

--- packages/client/src/routes/home/home.tsx
+++ packages/client/src/routes/home/home.tsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-// import { db, IFolder, IFolderCreate } from "../../db";
-// import { useLiveQuery } from "dexie-react-hooks";
-// import Accordion from '../../components/accordion/Accordion'
-// import {loadEnv} from 'vite'
-/*
-https://web.dev/file-system-access/
-https://web.dev/browser-fs-access/
-*/
+import logo from "/cube-purple.svg";
 import "./home.css";
 
 const Home = () => {
@@ -18,15 +11,28 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <p>
-        {import.meta.env.VITE_APP_NAME} is an app for managing you 3D model
+    <div className="home">
+      <div className="hero">
+        <h1  className="hero__text">MaDam3D</h1>
+        <img className="hero__logo" src={logo} alt="" />
+      </div>
+      <div  className="home__card">
+       <p>
+         
+         {import.meta.env.VITE_APP_NAME} is an app for managing you 3D model
         files, under the hood it uses the native file system access api, that as
         of December 2021 is available in{" "}
         <a href="https://caniuse.com/?search=file%20system%20access">
           Chrome and Edge &#62; 86 and the technical preview in Safari.
         </a>
-      </p>
+         </p>  
+      </div>
+      <div className="home__card"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum dignissimos optio quos molestias velit facilis aperiam, natus fugiat eligendi placeat accusantium. Optio, hic consectetur! Dolore velit nulla temporibus ab.</div>
+      <div className="home__card"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum dignissimos optio quos molestias velit facilis aperiam, natus fugiat eligendi placeat accusantium. Optio, hic consectetur! Dolore velit nulla temporibus ab.</div>
+      {/* <div className="home__card"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum dignissimos optio quos molestias velit facilis aperiam, natus fugiat eligendi placeat accusantium. Optio, hic consectetur! Dolore velit nulla temporibus ab.</div>
+      <div className="home__card"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum dignissimos optio quos molestias velit facilis aperiam, natus fugiat eligendi placeat accusantium. Optio, hic consectetur! Dolore velit nulla temporibus ab.</div> */}
+      
+      
     </div>
   );
 };
